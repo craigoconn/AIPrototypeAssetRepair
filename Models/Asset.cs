f4eb5120eb76f9867e2b16dde112ca3748fd2949
Craig O'Connor <craig.o'connor@bradyplc.com>
2025-06-04 16:27:06 +0100

    Add project files.

---
+++ Models/Asset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIPrototypeAssetRepair.Models
+{
+    public class Asset
+    {
+        public string AssetId { get; set; }
+        public string AssetType { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Models/Contractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIPrototypeAssetRepair.Models
+{
+    public class Contractor
+    {
+        public string Name { get; set; }
+        public string BaseLocation { get; set; }
+        public List<string> Skills { get; set; }
+        public DateTime Availability { get; set; }
+    }
+}
